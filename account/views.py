730c665366e5af171dbd17384c516c0762916022
sanjay parajuli <sanjayparajuli343@gmail.com>
2023-07-11 16:01:47 +0545

    Login and logout is implement using auth_views

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
-
+from django.contrib.auth.decorators import login_required
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -22,3 +22,8 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html',
+                  {'section': 'dashboard'})
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -105,9 +105,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
